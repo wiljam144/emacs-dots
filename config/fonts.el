2bfd6f7ae1b77ba8253e2f0d082cc8313a324161
Wiljam Holmes <wiljam2016@gmail.com>
2025-04-14 17:15:45 +0200

    Refactor fonts.

---
+++ config/fonts.el
@@ -0,0 +1,22 @@
+;; -*- coding: utf-8; lexical-binding: t -*-
+
+(setq wl/darwin-font-sizes 
+  '(:fixed 150 
+    :variable 150))
+
+(setq wl/other-font-sizes 
+  '(:fixed 120 
+    :variable 130))
+
+(defun wl/get-font-size (symbol)
+  (if (eq system-type 'darwin)
+    (plist-get wl/darwin-font-sizes symbol)
+    (plist-get wl/other-font-sizes symbol)))
+
+(custom-theme-set-faces
+  'user
+  `(default ((t :family "Fira Code" :height ,(wl/get-font-size :fixed))))
+  `(fixed-pitch ((t (:family "Fira Code" :height ,(wl/get-font-size :fixed)))))
+  `(variable-pitch ((t (:family "Literata" :height ,(wl/get-font-size :variable))))))
+
+(add-hook 'text-mode-hook 'variable-pitch-mode)
--- config/gui.el
+++ config/gui.el
@@ -1,5 +1,7 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
+(load "~/.config/emacs/config/fonts.el")
+
 ;; I'm an (emacs)-vim user afterall, so relative numbering it is.
 (setq display-line-numbers-type 'relative)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
@@ -14,18 +16,4 @@
 ;; And not attached to the window.
 (if (eq system-type 'darwin)
   (menu-bar-mode 1)
-    (menu-bar-mode -1))
-
-(defun wl/fonts-darwin ()
-  (progn
-    (add-to-list 'default-frame-alist '(font . "Fira Code 15"))
-    (set-face-attribute 'default t :font "Fira Code 15")))
-
-(defun wl/fonts-other ()
-  (progn
-    (add-to-list 'default-frame-alist '(font . "Fira Code 12"))
-    (set-face-attribute 'default t :font "Fira Code 12")))
-
-(if (eq system-type 'darwin)
-  (wl/fonts-darwin)
-  (wl/fonts-other))
+  (menu-bar-mode -1))
