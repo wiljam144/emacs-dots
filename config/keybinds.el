b061910396af5a13d0a76b23e452e75831c27f92
Wiljam Holmes <wiljam2016@gmail.com>
2025-04-15 22:54:11 +0200

    Refactor keybinds.

--- config/keybinds.el
+++ config/keybinds.el
@@ -11,14 +11,17 @@
   :ensure t
   :init
   (setq evil-want-keybinding nil
-        ;; I am morally against the existence of a way to enter into inferior Emacs default bindings.
+        evil-respect-virtual-line-mode 1
+        ;; I am morally against the existence of a way to enter
+        ;; into inferior Emacs default bindings.
         evil-toggle-key "")
+  :custom
+  (evil-want-C-u-scroll t)
+  (evil-want-fine-undo t)
+  (evil-undo-system 'undo-redo)
   :config
   (evil-mode 1)
-  (evil-set-leader nil (kbd "SPC"))
-  (setq evil-want-C-u-scroll t
-        evil-want-fine-undo t
-        evil-undo-system "undo-redo"))
+  (evil-set-leader nil (kbd "SPC")))
 
 (use-package evil-collection
   :after evil
@@ -26,72 +29,87 @@
   :config
   (evil-collection-init))
 
-;; Keybinds for buffer management.
-(evil-define-key 'normal 'global (kbd "<leader>B") 'switch-to-buffer)
-(evil-define-key 'normal 'global (kbd "<leader>.") 'next-buffer)
-(evil-define-key 'normal 'global (kbd "<leader>,") 'previous-buffer)
+(defun wl/binds-nm (list)
+  (dolist (bind list)
+    (evil-define-key 'normal 'global (kbd (car bind)) (eval (cdr bind)))))
 
-;; Keybinds for bookmarks
-(evil-define-key 'normal 'global (kbd "<leader>b s") 'bookmark-set)
-(evil-define-key 'normal 'global (kbd "<leader>b j") 'bookmark-jump)
+(defun wl/binds-in (list)
+  (dolist (bind list)
+    (evil-define-key 'insert 'global (kbd (car bind)) (eval (cdr bind)))))
 
-;; Dired
-(evil-define-key 'normal 'global (kbd "<leader>e") 'wl/toggle-dired-sidebar)
-(evil-define-key 'normal 'global (kbd "<leader>E") 'wl/open-dired)
+(setq wl/bindings-nm
+      ;; Buffer management.
+      '(("<leader>B" . 'switch-to-buffer)
+        ("<leader>." . 'next-buffer)
+        ("<leader>," . 'previous-buffer)
 
-;; Help system
-(evil-define-key 'normal 'global (kbd "<leader>h i") 'info)
-(evil-define-key 'normal 'global (kbd "<leader>h a") 'apropos)
-(evil-define-key 'normal 'global (kbd "<leader>h v") 'describe-variable)
-(evil-define-key 'normal 'global (kbd "<leader>h f") 'describe-function)
-(evil-define-key 'normal 'global (kbd "<leader>h s") 'describe-symbol)
-(evil-define-key 'normal 'global (kbd "<leader>h m") 'wl/woman)
+        ;; Bookmarks.
+        ("<leader>b s" . 'bookmark-set)
+        ("<leader>b j" . 'bookmark-jump)
 
-;; Window management
-(evil-define-key 'normal 'global (kbd "<leader>w v") 'evil-window-vsplit)
-(evil-define-key 'normal 'global (kbd "<leader>w h") 'evil-window-split)
-(evil-define-key 'normal 'global (kbd "C-h") 'windmove-left)
-(evil-define-key 'normal 'global (kbd "C-l") 'windmove-right)
-(evil-define-key 'normal 'global (kbd "C-j") 'windmove-down)
-(evil-define-key 'normal 'global (kbd "C-k") 'windmove-up)
+        ;; Dired.
+        ("<leader>e" . 'wl/toggle-dired-sidebar)
+        ("<leader>E" . 'wl/open-dired)
 
-;; Lisp
-(evil-define-key 'normal 'global (kbd "<leader>l e") 'eval-buffer)
+        ;; Help system.
+        ("<leader>h i" . 'info)
+        ("<leader>h a" . 'apropos)
+        ("<leader>h v" . 'describe-variable)
+        ("<leader>h f" . 'describe-function)
+        ("<leader>h s" . 'describe-symbol)
+        ("<leader>h m" . 'wl/woman)
 
-;; misc (tm).
-;(evil-define-key 'insert 'global (kbd "TAB") 'indent-according-to-mode)
-(evil-define-key 'insert 'global (kbd "TAB") (lambda () (interactive) (treesit-indent)))
-(evil-define-key 'insert 'global (kbd "<backtab>") (lambda () (interactive) (indent-according-to-mode)))
+        ;; Window management.
+        ("<leader>w v" . 'evil-window-vsplit)
+        ("<leader>w h" . 'evil-window-split)
+        ("C-h" . 'windmove-left)
+        ("C-l" . 'windmove-right)
+        ("C-j" . 'windmove-down)
+        ("C-k" . 'windmove-up)
 
-(evil-define-key 'normal 'global (kbd "SPC p") 'projectile-command-map)
+        ;; Lisp.
+        ("<leader>l b" . 'eval-buffer)
+        ("<leader>l e" . 'eval-expression)
 
-;; Org-mode
-(evil-define-key 'normal 'global (kbd "<leader>o e")
-  (lambda ()
-    (interactive)
-    (if (eq major-mode 'org-mode)
-      (org-edit-src-code)
-      (org-edit-src-exit))))
+        ;; Projectile.
+        ("SPC p" . 'projectile-command-map)
 
-;; This binding is straight up stolen from Doom Emacs.
-;; It's the best keybind I've written, it has so much functionality
-;; so neatly packed in it.
-(evil-define-key 'normal 'global (kbd "SPC SPC")
-  (lambda ()
-    (interactive)
-    (if (projectile-project-p)
-      (projectile-find-file)
-      (projectile-switch-project))))
+        ;; Org-mode.
+        ("<leader>o e" .
+         (lambda ()
+           (interactive)
+           (if (eq major-mode 'org-mode)
+               (org-edit-src-code)
+             (org-edit-src-exit))))
 
-;; To be honest I don't really like any of the
-;; Emacs's terminal options, hence I use kitty in another DE window.
-(evil-define-key 'normal 'global (kbd "<leader>`")
-  (lambda ()
-    (interactive)
-    (let ((filepath (if (buffer-file-name)
-                      (file-name-directory (buffer-file-name))
-                      "/home/wiljam")))
-      (start-process "kitty" nil "kitty" filepath))))
+        ;; This binding is straight up stolen from Doom Emacs.
+        ;; It's the best keybind I've written, it has so much functionality
+        ;; so neatly packed in it.
+        ("SPC SPC" .
+         (lambda ()
+           (interactive)
+           (if (projectile-project-p)
+               (projectile-find-file)
+             (projectile-switch-project))))
+
+
+        ;; To be honest I don't really like any of the
+        ;; Emacs's terminal options, hence I use kitty in another DE window.
+        ("<leader>`" .
+         (lambda ()
+           (interactive)
+           (let ((filepath (if (buffer-file-name)
+                               (file-name-directory (buffer-file-name))
+                             "/home/wiljam")))
+             (start-process "kitty" nil "kitty" filepath))))))
+
+(setq wl/bindings-in
+      ;; Indentation.
+      '(("TAB" . (lambda () (interactive) (treesit-indent)))
+        ("<backtab>" . (lambda () (interactive) (indent-according-to-mode)))))
+
+(wl/binds-nm wl/bindings-nm)
+(wl/binds-in wl/bindings-in)
 
 ;; This will infuriate a lot of people...
 ;; It is my personal act of defiance against the Emacs bindings
@@ -134,13 +152,13 @@
                  "C-`" "C-~" "C-!" "C-@" "C-#" "C-$" "C-%" "C-^" "C-&" "C-*"
                  "C-(" "C-)" "C--" "C-=" "C-+" "C-]" "C-\\" "C-|" "C-;" ;; C-[ not included because it is ESC.
                  "C-:" "C-'" "C-\"" "C-," "C-." "C-<" "C->" "C-/" "C-?" "C-_")) ())
-    ;(define-key wl/intercept-keymap (kbd key) #'wl/ignore-key))
+                                        ;(define-key wl/intercept-keymap (kbd key) #'wl/ignore-key))
 
   (dolist (special-key '("SPC" "TAB" "ESC" "DEL" "<home>" "<end>" "<prior>" "<next>"
-                          "<left>" "<right>" "<up>" "<down>" "<insert>" "<delete>"
-                          "<tab>" "<backspace>"
-                          "<f1>" "<f2>" "<f3>" "<f4>" "<f5>" "<f6>"
-                          "<f7>" "<f8>" "<f9>" "<f10>" "<f11>" "<f12>"))
+                         "<left>" "<right>" "<up>" "<down>" "<insert>" "<delete>"
+                         "<tab>" "<backspace>"
+                         "<f1>" "<f2>" "<f3>" "<f4>" "<f5>" "<f6>"
+                         "<f7>" "<f8>" "<f9>" "<f10>" "<f11>" "<f12>"))
     (define-key wl/intercept-keymap (kbd (concat "C-" special-key)) #'wl/ignore-key))
 
   ;; Make sure our keymap is higher priority than all others
@@ -157,8 +175,8 @@
 (wl-intercept-mode 1)
 
 (setq emulation-mode-map-alists
-  (append '(evil-mode-map-alist)
-    (remove 'evil-mode-map-alist emulation-mode-map-alists)))
+      (append '(evil-mode-map-alist)
+              (remove 'evil-mode-map-alist emulation-mode-map-alists)))
 
 (add-to-list 'emulation-mode-map-alists
              `((wl-intercept-mode . ,wl/intercept-keymap))
