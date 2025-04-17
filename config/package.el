cf2fc8b1a06af61158f8601ad6822601de865369
Wiljam Holmes <wiljam2016@gmail.com>
2025-04-17 23:33:29 +0200

    Remove tramp file.

--- config/package.el
+++ config/package.el
@@ -1,5 +1,6 @@
 ;; -*- coding: utf-8; lexical-binding: t -*-
 
+(setq use-package-compute-statistics t)
 (require 'package)
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
